9a8c53ea4fcf25abdd790d2b19ea097eae96b72c
Misha Panivnyk <panivnykm@gmail.com>
2022-12-25 14:27:32 +0200

    add BackLink

---
+++ src/components/BackLink/BackLink.jsx
@@ -0,0 +1,17 @@
+import { HiArrowLeft } from 'react-icons/fa';
+import PropTypes from 'prop-types';
+import { StyledLink } from './BackLink.styled';
+
+const BackLink = ({ to, children }) => {
+  return (
+    <StyledLink to={to}>
+      <HiArrowLeft size="24" />
+      {children}
+    </StyledLink>
+  );
+};
+export default BackLink;
+BackLink.propTypes = {
+  to: PropTypes.object,
+  children: PropTypes.string,
+};
