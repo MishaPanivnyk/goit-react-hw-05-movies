0743ae85e5235dc06fe00c7aaebccb31fe3780f2
Misha Panivnyk <panivnykm@gmail.com>
2022-12-25 18:27:43 +0200

    add Reviews

---
+++ src/components/MovieDetails/Reviews.jsx
@@ -0,0 +1,29 @@
+import getMovieReviews from '../../services/getMovieReviews';
+import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+
+const Reviews = () => {
+  const { movieId } = useParams();
+  const [reviwsHtml, setReviewsHtml] = useState('');
+  const [reviewLength, setRewiewsLength] = useState(0);
+  useEffect(() => {
+    getMovieReviews(movieId).then(movieData => {
+      setReviewsHtml(
+        movieData.data.results.map(reviewEl => (
+          <li key={reviewEl.id}>
+            <h3>Author: {reviewEl.author}</h3>
+            <p>'{reviewEl.content}'</p>
+          </li>
+        ))
+      );
+      setRewiewsLength(movieData.data.results.length);
+    });
+  }, [movieId]);
+  return (
+    <section>
+      <h2>Reviews</h2>
+      <ul>{reviewLength !== 0 ? reviwsHtml : 'No reviews for this movie'}</ul>
+    </section>
+  );
+};
+export default Reviews;
