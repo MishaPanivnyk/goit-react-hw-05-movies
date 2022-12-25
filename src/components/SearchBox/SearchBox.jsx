ac0760812b2ec182c97ad9533dc048947d5a9f86
Misha Panivnyk <panivnykm@gmail.com>
2022-12-25 18:27:52 +0200

    add SearchBox

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,7 +1,24 @@
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 import { Wrapper, Input, Icon, Button } from './SearchBox.styled';
 export const SearchBox = ({ value, onChange, onClick }) => {
-    return (
-        
-    )
-}
+  return (
+    <Wrapper>
+      <Input
+        type="text"
+        placeholder="Enter movie name"
+        value={value}
+        onChange={e => {
+          onChange(e.target.value);
+        }}
+      />
+      <Button type="button" onClick={onClick}>
+        <Icon />
+      </Button>
+    </Wrapper>
+  );
+};
+SearchBox.propTypes = {
+  value: PropTypes.string,
+  onChange: PropTypes.func,
+  onClick: PropTypes.func,
+};
