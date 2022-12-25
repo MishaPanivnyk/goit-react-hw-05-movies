9f2f0a7bd1ff914abafa43394ac39a820ef88ace
Misha Panivnyk <panivnykm@gmail.com>
2022-12-25 14:27:58 +0200

    add SearchBox

---
+++ src/components/SearchBox/SearchBox.jsx
@@ -0,0 +1,7 @@
+import PropTypes from 'prop-types'
+import { Wrapper, Input, Icon, Button } from './SearchBox.styled';
+export const SearchBox = ({ value, onChange, onClick }) => {
+    return (
+        
+    )
+}
