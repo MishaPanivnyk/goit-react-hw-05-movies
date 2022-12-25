4efc2373c51bfdf26cef39571a5ed7717c7eff8a
Misha Panivnyk <panivnykm@gmail.com>
2022-12-25 18:27:33 +0200

    add MovieInfo

---
+++ src/components/MovieDetails/MovieInfo.styled.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: inline-flex;
+  width: auto;
+`;
+
+export const CardWrapper = styled.div`
+  display: block;
+  padding: 30px;
+
+  > a {
+    text-decoration: none;
+  }
+`;
+export const ImageBlock = styled.img`
+  display: inline-flex;
+  width: 30%;
+`;
+export const Header1 = styled.h1`
+  padding: 0;
+  margin: 0;
+  border: 0;
+`;
